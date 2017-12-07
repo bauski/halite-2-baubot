7227a68cdea1b42c83af0b9bd8ba422af80a2bb1
bauski <baku.kim@gmail.com>
2017-12-06 20:49:06 -0800

    Redid code to be much more efficient. Added space for attacking on targets.

--- strategies/strategies.js
+++ strategies/strategies.js
@@ -1,206 +1,95 @@
 const constants = require('../hlt/Constants');
 const Geometry = require('../hlt/Geometry');
 
-/**
- * strategy is a function that accepts the current game map and return a list of next steps to take
- * @param {GameMap} gameMap
- * @returns {string[]} moves that needs to be taken. null values are ignored
- */
 // baubot 4 target strat.
-// Change target preferences depending on how many planets we have.
-function fourStrat(gameMap) {
-    // Get all planets
+function basic(gameMap) {
     var planets = gameMap.planets;
-    // Set distance Buffer variables
-    dockedEnemyBuffer = 5;
-    enemyBuffer = 15;
-    dockBuffer = 15;
-    freeBuffer = 15;
-    // Ship Commands
+    var enemies = gameMap.enemyShips;
+    var targetPlanet = false;
+    var targetEnemy = false;
+
+    var planetBuffer = 15;
+    var enemyBuffer = 5;
+
+    // Ship Tactics.
     const moves = gameMap.myShips.filter(s => s.isUndocked()).map(
     ship => {
-        // Get my planets.
-        var myPlanets = planets.filter(p => p.isOwnedByMe());
-        if(myPlanets < 3) {
-            enemyBuffer = 15;
-            dockBuffer = 15;
-            freeBuffer = 35;
-        } else {
-            enemyBuffer = 15;
-            dockBuffer = 15;
-            freeBuffer = 15;
-        }
+        var dockablePlanets = planets.filter(p => p.isOwnedByMe() && p.hasDockingSpot());
+        var dockablePlanet = getClosest(dockablePlanets,ship);
 
-        // Look for a close free planet.
-        var freePlanets = planets.filter(p => p.isFree()).sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b));
-        var freePlanet = freePlanets[0];
+        var freePlanets = planets.filter(p => p.isFree());
+        var freePlanet = getClosest(freePlanets,ship);
 
-        // Look for dockable planets.
-        var dockPlanets = planets.filter(p => (p.isOwnedByMe() && p.hasDockingSpot())).sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b));
-        var dockPlanet = dockPlanets[0];
+        var activeEnemies = enemies.filter(s => s.isUndocked());
+        var activeEnemy = getClosest(activeEnemies,ship);
 
-        // Look for enemy ship.
-        var enemies = gameMap.enemyShips;
-        enemies = [...enemies].sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b));
-        var enemy = enemies[0];
+        var busyEnemies = enemies.filter(s => !s.isUndocked());
+        var busyEnemy = getClosest(busyEnemies,ship);
 
-        // Looke for vulnerable enemy ships.
-        var dockedEnemies = enemies.filter(e => !e.isUndocked());
-        dockedEnemies = [...enemies].sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b));
-        var docked = dockedEnemies[0];
+        // Set target planet if possible.
+        targetPlanet = getTarget(freePlanet, dockablePlanet, planetBuffer, ship);
+        // Set target enemy if possible.
+        targetEnemy = getTarget(busyEnemy, activeEnemy, enemyBuffer, ship);
 
-        if(freePlanets.length > 0 && dockPlanets.length > 0) {
-            if (
-                ship.distanceBetween(freePlanet) <= ship.distanceBetween(dockPlanet) + dockBuffer &&
-                ship.distanceBetween(freePlanet) < ship.distanceBetween(enemy) + enemyBuffer
-            ) {
-                if (ship.canDock(freePlanet)) {
-                    return ship.dock(freePlanet);
-                } else {
-                    return ship.navigate({
-                        target: freePlanet,
-                        keepDistanceToTarget: freePlanet.radius + 3,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
-            } else if (
-                ship.distanceBetween(dockPlanet) < ship.distanceBetween(freePlanet) - freeBuffer &&
-                ship.distanceBetween(dockPlanet) < ship.distanceBetween(enemy) + enemyBuffer
-            ) {
-                if (ship.canDock(dockPlanet)) {
-                    return ship.dock(dockPlanet);
-                } else {
-                    return ship.navigate({
-                        target: dockPlanet,
-                        keepDistanceToTarget: dockPlanet.radius + 3,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
+        if(targetPlanet && targetEnemy) {
+            if(ship.distanceBetween(targetPlanet) < ship.distanceBetween(targetEnemy)) {
+                return tryDocking(ship,targetPlanet,move);
             } else {
-                if (ship.distanceBetween(docked) < ship.distanceBetween(enemy) + dockedEnemyBuffer) {
-                    return ship.navigate({
-                        target: docked,
-                        keepDistanceToTarget: 5,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                } else {
-                    return ship.navigate({
-                        target: enemy,
-                        keepDistanceToTarget: 5,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
+                return move(ship,targetEnemy);
             }
-        } else if(freePlanets.length > 0 && dockPlanets.length === 0) {
-            if (ship.distanceBetween(freePlanet) < ship.distanceBetween(enemy) + enemyBuffer) {
-                if (ship.canDock(freePlanet)) {
-                    return ship.dock(freePlanet);
-                } else {
-                    return ship.navigate({
-                        target: freePlanet,
-                        keepDistanceToTarget: freePlanet.radius + 3,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
-            } else {
-                if (ship.distanceBetween(docked) < ship.distanceBetween(enemy) + enemyBuffer) {
-                    return ship.navigate({
-                        target: docked,
-                        keepDistanceToTarget: 5,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                } else {
-                    return ship.navigate({
-                        target: enemy,
-                        keepDistanceToTarget: 5,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
-            }
-        }  else if(dockPlanets.length > 0 && freePlanets.length === 0) {
-            if (ship.distanceBetween(dockPlanets) < ship.distanceBetween(enemy) + enemyBuffer) {
-                if (ship.canDock(dockPlanets)) {
-                    return ship.dock(dockPlanets);
-                } else {
-                    return ship.navigate({
-                        target: dockPlanet,
-                        keepDistanceToTarget: dockPlanet.radius + 3,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
-            } else {
-                if (ship.distanceBetween(docked) < ship.distanceBetween(enemy) + dockedEnemyBuffer) {
-                    return ship.navigate({
-                        target: docked,
-                        keepDistanceToTarget: 5,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                } else {
-                    return ship.navigate({
-                        target: enemy,
-                        keepDistanceToTarget: 5,
-                        speed: constants.MAX_SPEED,
-                        avoidObstacles: true,
-                        ignoreShips: false
-                    });
-                }
-            }
-        } else {
-            if (ship.distanceBetween(docked) < ship.distanceBetween(enemy) + dockedEnemyBuffer) {
-                return ship.navigate({
-                    target: docked,
-                    keepDistanceToTarget: 5,
-                    speed: constants.MAX_SPEED,
-                    avoidObstacles: true,
-                    ignoreShips: false
-                });
-            } else {
-                return ship.navigate({
-                    target: enemy,
-                    keepDistanceToTarget: 5,
-                    speed: constants.MAX_SPEED,
-                    avoidObstacles: true,
-                    ignoreShips: false
-                });
-            }
+        }  else if(targetPlanet && !targetEnemy) {
+            return tryDocking(ship,targetPlanet,move);
+        } else if(!targetPlanet && targetEnemy) {
+            return move(ship,targetEnemy);
         }
     });
     return moves;
-}
-// baubot focus strat.
-function focusStrat(gameMap) {
-    var targetPlanet;
 
-    const moves = gameMap.myShips.filter(s => s.isUndocked()).map(
-    ship => {
-        // If targetPlanet is not set, find nearest planet and set to targetPlanet.
-            // Head for planet.
-        // Else
-            // If distance from ship to planet is less than 5.
-                // If planet dockable, dock.
-                // Else find all vulnerable enemies.
-                    // If there are vulnerable enemies, kill.
-                    // Else, find next
-            // Else
-    });
-    return moves;
+    function getClosest(multiple,ship) {
+        if(multiple.length > 0) {
+            return multiple.sort((a, b) => Geometry.distance(ship, a) - Geometry.distance(ship, b))[0];
+        } else {
+            return false;
+        }
+    }
+
+    function tryDocking(ship,planet,move) {
+        if(ship.canDock(planet)) {
+            return ship.dock(planet);
+        } else {
+            return move(ship,planet);
+        }
+    }
+
+    function getTarget(preferred,secondary,buffer,ship) {
+        var target = false;
+        if(preferred && secondary) {
+            if(ship.distanceBetween(preferred) < ship.distanceBetween(secondary) + buffer) {
+                target = preferred;
+            } else {
+                target = secondary;
+            }
+        } else if(preferred && !secondary) {
+            target = preferred;
+        } else if(!preferred && secondary) {
+            target = secondary;
+        }
+        return target;
+    }
+
+    function move(ship,target) {
+        var speed = constants.MAX_SPEED;
+        var distance = 5;
+        if(target.toString()[0] == "p") {
+            distance = target.radius + 3;
+        }
+        return ship.navigate({
+            target: target,
+            keepDistanceToTarget: distance,
+            speed: speed,
+            avoidObstacles: true,
+            ignoreShips: false
+        });
+    }
 }
-module.exports = {fourStrat,focusStrat};
+module.exports = {basic};
